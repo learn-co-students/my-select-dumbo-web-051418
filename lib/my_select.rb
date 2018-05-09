59beadc2b46195179400a36e4a356d8b16f41e0d
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-09 14:26:38 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    selected_collection = []
+    i = 0
+    while i < collection.length do 
+      if yield(collection[i]) == true 
+        selected_collection << collection[i]
+      end 
+      i+=1
+    end
+    return selected_collection
+  else 
+    puts "Provide block please"
+  end 
 end
